068fb58d8d18b332529ecaf1b7eed23a24633653
liu <32328254@qq.com>
2018-05-20 00:09:17 +0800

    add create file

--- day04/create_file.py
+++ day04/create_file.py
@@ -2,14 +2,36 @@
 # -*- coding:utf-8 -*-
 import os
 
+
 def get_filename():
     while True:
         file_name = input('please input you file name: ')
         if not os.path.exists(file_name):
-            return file_name
+            #return file_name
+            get_info(file_name)
             break
         else:
-            print(file_name,"exits,please input age!!!")
+            print(file_name, "exits,please try input age!!!")
+
+def get_info(filename):
+    info_list = []
+    while True:
+        data = input("please input you information[input q is quit]: ")
+        if not data == "q":
+            info_list.append(data)
+        else:
+            print("thanks you used")
+            print(info_list)
+            with open(filename,'w+') as f:
+                for i in info_list:
+                    i = i + '\n'
+                    f.write(i)
+                #f.writelines(info_list)
+            break
+
+
 if __name__ == '__main__':
-    a = get_filename()
-    print(a)
+    #a = get_filename()
+    #print(a)
+    get_filename()
+    #get_info('liu.txt')
