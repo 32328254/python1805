bc7a4abfc5f3f2ba915da5f69a2f833346cf82ad
root <root@room9pc01.tedu.cn>
2018-06-10 10:47:22 +0800

    add backfile

--- day10/backfile.py
+++ day10/backfile.py
@@ -4,28 +4,67 @@ import time
 import tarfile
 import hashlib
 import os
-import sys
-md5file = '/tmp/md5file.dat'
-local_time = time.strftime('%Y-%m-%d')
+import mymd5sum
+import pickle
 
-def full_back(src_dir,dest_dir):
-    sfile = os.path.basename(src_dir.rstrip("/")) #获取原文件名
-    #print(sfile)
-    dest_dir = dest_dir.rstrip("/")#获取目标目录
-    print(dest_dir)
-    bfile = "%s-%s.tar.gz"%(sfile,local_time)     #备份文件名
-    print(bfile)
-    dst_file = os.path.join(dest_dir,bfile)       #拼接路径
-    print(dst_file)
-    file = tarfile.open(dst_file,'w:gz')
+md5file = '/tmp/md5file.dat'
+local_time = time.strftime('%Y-%m-%d-%H-%M-%S')
+
+
+def full_back(src_dir, dest_dir):
+    sfile = os.path.basename(src_dir.rstrip("/"))  # 获取原文件名
+    # sdir = os.path.dirname(src_dir)
+    # print(sdir)
+    # print(sfile)
+    dest_dir = dest_dir.rstrip("/")  # 获取目标目录
+    # print(dest_dir)
+    bfile = "%s-full-%s.tar.gz" % (sfile, local_time)  # 备份文件名
+    # print(bfile)
+    dst_file = os.path.join(dest_dir, bfile)  # 拼接路径
+    # print(dst_file)
+    file = tarfile.open(dst_file, 'w:gz')
     file.add(src_dir)
     file.close()
 
-def incrback():
-    pass
+    md5dict = {}
+    for path, dir, file in os.walk(src_dir):  # os.walk(path_dir) path_dir是个路径
+        for i in file:
+            fn = os.path.join(path, i)
+            print(fn)
+            md5dict[fn] = mymd5sum.check_md5(fn)
+    print(md5dict)
+
+    with open(md5file, 'wb') as fd:
+        pickle.dump(md5dict, fd)
+
+
+def incrback(src_dir, dest_dir):
+    sfile = os.path.basename(src_dir.rstrip("/"))  # 获取原文件名
+    dest_dir = dest_dir.rstrip("/")  # 获取目标目录
+    bfile = "%s-incr-%s.tar.gz" % (sfile, local_time)  # 备份文件名
+    dst_file = os.path.join(dest_dir, bfile)  # 拼接路径
+
+    md5dict = {}
+    for path, dir, file in os.walk(src_dir):  # os.walk(path_dir) path_dir是个路径
+        for i in file:
+            fn = os.path.join(path, i)
+            print(fn)
+            md5dict[fn] = mymd5sum.check_md5(fn)
+    with open(md5file, 'rb') as fd:
+        old_md5dict = pickle.load(fd)
+    tar = tarfile.open(dst_file, 'w:gz')
+    for k in md5dict:
+        if old_md5dict.get(k) != md5dict[k]:
+            tar.add(k)
+    tar.close()
+
+    with open(md5file) as fd:
+        pickle.dump(md5dict,fd)
+
 
 if __name__ == '__main__':
-    if time.strftime('%a') == 'Sat':
-        full_back('/etc/passwd','/tmp')
+    # print(time.strftime('%a'))
+    if time.strftime('%a') == 'SuN':
+        full_back('/home/liu/day10', '/tmp')
     else:
-        incrback()
+        incrback('/home/liu/day10', '/tmp')
