41115747dc0e3f3207c3f58d4d5bbad3cc0f31ae
root <root@room9pc01.tedu.cn>
2018-06-10 17:34:48 +0800

    add start server

--- day11/start_server.py
+++ day11/start_server.py
@@ -25,23 +25,37 @@ class Process:
             os.chdir(self.workdir)
             os.system("chown memcached %s" % self.workdir)
 
+    def _get_pid(self):
+        pid = Popen('pidof %s' % self.name, stdout=PIPE, stderr=PIPE, shell=True)
+        return pid.stdout.read().strip()
+
     def start(self):
         self._init()
         cmd = self.program + ' ' + self.args + ' ' + '- P %s.pid' % os.path.join(self.workdir, self.name)
         p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
         if not p.stderr.read():  # 如果程序正常启动,err是null,if null 是条件不成立的,加上not 是条件成立.
-            print("%s start successful" % self.name)
+            print("%s started successful" % self.name)
 
     def stop(self):
-        pass
+        pid = self._get_pid()
+        if pid:  #有值则执行
+            os.kill(int(pid),15)
+            self.status()
+        else:
+            print("%s stoped "%self.name)
 
     def restart(self):
         self.stop()
         self.start()
 
     def status(self):
-        pass
-
+        pid = self._get_pid()
+        if pid:  #有值则执行
+            print("%s is runing"%self.name)
+        else:
+            print("%s stoped "%self.name)
+    def help(self):
+        print("Usage: %s [start/stop/restart/status]"%sys.argv[0])
 
 if __name__ == '__main__':
     workdir = '/var/tmp/memcached'
@@ -49,7 +63,11 @@ if __name__ == '__main__':
     program = '/usr/bin/memcached'
     args = '-u memcached -p 11211 -m 64 -c 1024 -d'
     pm = Process(workdir=workdir, name=name, program=program, args=args)
-    cmd = sys.argv[1]
+    try:
+        cmd = sys.argv[1]
+    except IndexError:
+        pm.help()
+        sys.exit(1)
     if cmd == 'start':
         pm.start()
     elif cmd == 'stop':
@@ -60,3 +78,4 @@ if __name__ == '__main__':
         pm.status()
     else:
         print("Invaid argment")
+        pm.help()
