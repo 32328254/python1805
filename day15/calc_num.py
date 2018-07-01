f656610908e574f7bc5fd25041df5e1ee530c989
root <root@room9pc01.tedu.cn>
2018-07-01 09:33:47 +0800

    add day15

---
+++ day15/calc_num.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import time
+import os
+
+def func():
+    result = 0
+    for i in range(50000000):
+        result += i
+    print(result)
+
+if __name__ == '__main__':
+    start = time.time()
+    for i in range(2):
+        pid = os.fork()
+        if not pid:
+            func()
+            exit(1)
+    os.waitpid(-1, 0)
+    os.waitpid(-1, 0)
+    end = time.time()
+    print(end - start)
---
+++ day15/my_thread.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+thread和threading模块允许程序员创建和管理线程,
+thread模块提供了基本的线程和锁的支持,而threading提供了更高级别,功能更强的线程管理功能
+推荐使用更高级别的threading模块
+
+"""
